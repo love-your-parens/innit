3ace347bd2c4a19d1ce6869ce60db3f4a193540c
Konrad Wątor <konrad@wator.it>
2025-03-22 18:34:55 +0100

    Let's jam

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to innit/innit
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/innit/innit_test.clj
@@ -0,0 +1,7 @@
+(ns innit.innit-test
+  (:require [clojure.test :refer :all]
+            [innit.innit :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
